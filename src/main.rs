1023375e789d2bc7101d451d690a1ec62ece3668
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-16 12:05:42 -0600

    feat: se implementaron cambios en el main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-mod framebuffer;
-use framebuffer::Framebuffer;
-
 mod color;
-use color::Color;
-
+mod framebuffer;
+mod sphere;
 mod texture;
-use texture::Texture;
+
+use crate::color::Color;
+use crate::framebuffer::Framebuffer;
+use crate::sphere::Sphere;
+use crate::texture::Texture;
+use nalgebra_glm::Vec3;
+
+// Definir el trait para los objetos que soportarán intersección de rayos
+pub trait RayIntersect {
+    fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> bool;
+}
+
+fn cast_ray(ray_origin: &Vec3, ray_direction: &Vec3, objects: &[Box<dyn RayIntersect>]) -> u32 {
+    for object in objects {
+        if object.ray_intersect(ray_origin, ray_direction) {
+            return 0xFFFFFF; // Color blanco si hay intersección
+        }
+    }
+    0x000000 // Color negro si no hay intersección
+}
 
 fn main() {
-    println!("Hello, world!");
+    let sphere = Sphere::new(Vec3::new(0.0, 0.0, -5.0), 1.0, Color::new(255, 0, 0));
+    let objects: Vec<Box<dyn RayIntersect>> = vec![Box::new(sphere)];
+
+    let ray_origin = Vec3::new(0.0, 0.0, 0.0);
+    let ray_direction = Vec3::new(0.0, 0.0, -1.0);
+
+    let color = cast_ray(&ray_origin, &ray_direction, &objects);
+    println!("Color resultante: {:06X}", color); // Debería imprimir "FFFFFF" si hay intersección
 }
