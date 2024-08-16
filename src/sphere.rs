c39c611324753154767a1665738f96056b0d42b7
SergioAle210 <ore221122@uvg.edu.gt>
2024-08-16 12:14:08 -0600

    feat: se modifico el archivo de la esfera con el material creado anteriormente

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,40 +1,42 @@
-use crate::color::Color;
-use crate::RayIntersect;
+use crate::ray_intersect::{Intersect, Material, RayIntersect};
 use nalgebra_glm::Vec3;
 
 pub struct Sphere {
     pub center: Vec3,
     pub radius: f32,
-    pub color: Color,
+    pub material: Material,
 }
 
 impl Sphere {
-    pub fn new(center: Vec3, radius: f32, color: Color) -> Self {
+    pub fn new(center: Vec3, radius: f32, material: Material) -> Self {
         Self {
             center,
             radius,
-            color,
+            material,
         }
     }
 }
 
 impl RayIntersect for Sphere {
-    fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> bool {
+    fn ray_intersect(&self, ray_origin: &Vec3, ray_direction: &Vec3) -> Intersect {
         let l = self.center - *ray_origin;
         let tca = l.dot(ray_direction);
         let d2 = l.dot(&l) - tca * tca;
         let radius2 = self.radius * self.radius;
+
         if d2 > radius2 {
-            return false;
+            return Intersect::empty();
         }
+
         let thc = (radius2 - d2).sqrt();
         let t0 = tca - thc;
         let t1 = tca + thc;
 
         if t0 < 0.0 && t1 < 0.0 {
-            return false;
+            return Intersect::empty();
         }
 
-        true
+        let distance = if t0 < 0.0 { t1 } else { t0 };
+        Intersect::new(distance, self.material.clone())
     }
 }
