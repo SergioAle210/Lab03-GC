c3f7b38e057971a9f88534ca634acce39870d28b
SergioAle210 <ore221122@uvg.edu.gt>
2024-09-22 11:33:37 -0600

    FEAT: se implemento la textura al material

--- src/material.rs
+++ src/material.rs
@@ -1,20 +1,31 @@
-use crate::color::Color;
+use std::sync::Arc;
 
-#[derive(Debug, Clone, Copy)]
+use crate::color::Color;
+use crate::texture::Texture;
+
+#[derive(Debug, Clone)]
 pub struct Material {
     pub diffuse: Color,
     pub specular: f32,
     pub albedo: [f32; 4],      // [difusa, especular, reflectiva, transparente]
     pub refractive_index: f32, // Índice de refracción
+    pub texture: Option<Arc<Texture>>, // Referencia a la textura
 }
 
 impl Material {
-    pub fn new(diffuse: Color, specular: f32, albedo: [f32; 4], refractive_index: f32) -> Self {
+    pub fn new(
+        diffuse: Color,
+        specular: f32,
+        albedo: [f32; 4],
+        refractive_index: f32,
+        texture: Option<Arc<Texture>>,
+    ) -> Self {
         Material {
             diffuse,
             specular,
             albedo,
             refractive_index,
+            texture,
         }
     }
 
@@ -24,6 +35,18 @@ impl Material {
             specular: 0.0,
             albedo: [0.0, 0.0, 0.0, 0.0],
             refractive_index: 1.0, // Índice de refracción del vacío
+            texture: None,         // Sin textura
+        }
+    }
+
+    // Función para obtener el color difuso de la textura
+    pub fn get_diffuse_color(&self, u: f32, v: f32) -> Color {
+        if let Some(texture) = &self.texture {
+            let x = (u * (texture.width - 1) as f32) as usize;
+            let y = (v * (texture.height - 1) as f32) as usize;
+            texture.get_pixel(x, y)
+        } else {
+            self.diffuse
         }
     }
 }
